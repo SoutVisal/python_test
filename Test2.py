5663fb9ca8c9a36ebde5f443b15fba3d05909f40
Visal Sout <soutvisal2020@gmail.com>
2024-07-12 10:31:02 +0700

    first commit

---
+++ Test2.py
@@ -0,0 +1,12 @@
+# weight = int(input("input Your weight= "))
+# height = float(input("Input Your height= "))
+# BMI = weight//height**2
+# welcome = "Hello student."
+# print(f'{welcome}Your BMI = {BMI}')
+print('=============== Welcome to the Tip Calculator=================')
+# total = float(input('What was the total bill? ='))
+# tip = float(input('How much tip would you like to give? 100 200 or more ='))
+# people = int(input('How maney people to splite the bill?'))
+# pay = (total + total*tip/100)/people
+# print(f'pay)
+
